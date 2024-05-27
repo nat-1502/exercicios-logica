ebe56e131c55c0c69da35bfc6e2ac6e735b5ca4d
nat-1502 <natalisoares90@outlook.com>
2024-05-27 19:43:45 -0300

    1 execicio de lógica

---
+++ ,.py
@@ -0,0 +1,13 @@
+
+
+salario =  input("Qual o seu salario? ")
+
+horas_por_dia = input("Quantas horas por dia você trabalha? ")
+
+dias_por_mes = input("Quantos dias por mês? ")
+
+valor_hora = int(salario) / int(dias_por_mes) / int(horas_por_dia)
+
+
+
+print (f'O seu valor hora é , {valor_hora:.2f}') 
