41f28a0d604841c76a6f3bb8f020a7762adca8fc
nat-1502 <natalisoares90@outlook.com>
2024-06-05 14:29:11 -0300

    exercicio resolvido

--- exercicio2.py
+++ exercicio2.py
@@ -1,12 +1,21 @@
 import random
 
 numero_aleatorio = random.randint(1, 10)
-
-print (numero_aleatorio)
 chute_usuario = input("Digite um numero de 0 a 10: ")
 
+
 chute_numero_int = int(chute_usuario)
+    
+if chute_numero_int == numero_aleatorio:
+        print ('Você acertou, parabéns!')
+    
+while chute_numero_int !=  numero_aleatorio :
+    print('Tente novamente, voce errou!')
+    chute_usuario = int(input("Digite um numero de 0 a 10: "))
 
-if chute_numero_int > 10 :
-    print ("Você digitou um numero maior que 10")
+    
+    if chute_usuario == numero_aleatorio:
+        print(f'Parabens voce acertou, o numero al2atório é {numero_aleatorio}!!')
+        break
 
+    
